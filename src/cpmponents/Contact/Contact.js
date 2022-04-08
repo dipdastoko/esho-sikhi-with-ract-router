719bbf0a7f32afad4d51974f175e441bdfe07bd1
dipdastoko <dipdastoko@gmail.com>
2022-04-08 20:53:28 +0600

    data updaed

--- src/cpmponents/Contact/Contact.js
+++ src/cpmponents/Contact/Contact.js
@@ -11,14 +11,18 @@ const Contact = () => {
             <div className='w-25 mx-auto'>
                 <h3>Your Name</h3>
                 <input placeholder='first name' type="text" /><span> </span>
-                <input placeholder='second name' type="text" />
+                <input placeholder='second name' type="text" /><br /><br />
+
                 <h3>Email Address</h3>
-                <input placeholder='e-mail' type="email" name="" id="" />
+                <input placeholder='e-mail' type="email" name="" id="" /><br /><br />
+
                 <h3>Phone Number</h3>
-                <input type="text" />
+                <input placeholder='01xxx' type="text" /><br /><br />
+
                 <h3>Message</h3>
                 <textarea placeholder='type your message here' name="" id="" cols="50" rows="10"></textarea>
                 <br />
+
                 <Button style={{ width: '150px', height: '50px', border: '2px solid gray', color: 'white' }} variant="primary" className='rounded-pill'>Submit <FontAwesomeIcon icon={faArrowRight} /></Button>
             </div>
         </div>
--- src/cpmponents/CourseDetails/CourseDetails.js
+++ src/cpmponents/CourseDetails/CourseDetails.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const CourseDetails = () => {
@@ -13,13 +14,14 @@ const CourseDetails = () => {
     const selectedLanguage = languages.find(language => language.id == id);
 
     return (
-        <div className='text-center'>
+        <div className='text-center p-4'>
             <h2>Course Name: Learning {selectedLanguage?.language}</h2><br />
             <img style={{ height: '350px', width: '350px' }} src={selectedLanguage?.img} alt="" />
             <h5>Trainer: {selectedLanguage?.trainer}</h5>
             <h5>Duration: {selectedLanguage?.duration}</h5>
             <h5>Fee: ${selectedLanguage?.fee}</h5><br />
             <blockquote>{selectedLanguage?.description}</blockquote>
+            <Button>Enroll Course</Button>
         </div>
     );
 };
